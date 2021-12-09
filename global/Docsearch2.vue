31457d122076c87159157e006af6c49cdf506b38
John Ouellet <john@thinktandem.io>
2021-12-09 13:13:01 -0500

    Add our own DocSearch component

---
+++ global/Docsearch2.vue
@@ -0,0 +1,69 @@
+<script>
+// @ts-ignore: docsearch type issue
+import docsearch from '@docsearch/js';
+import { usePageLang, useRouteLocale } from '@vuepress/client';
+import { computed, h, onMounted, watch } from 'vue';
+//import { useDocsearchShim } from '../composables';
+import '@docsearch/css';
+
+export default {
+  name: 'Docsearch2',
+  props: {
+      options: {
+          type: Object,
+          required: true,
+      },
+  },
+  setup(props) {
+    const routeLocale = useRouteLocale();
+    const lang = usePageLang();
+    //const docsearchShim = useDocsearchShim();
+    // resolve docsearch props for current locale
+    const propsLocale = computed(() => {
+        var _a;
+        return ({
+            ...props.options,
+            ...(_a = props.options.locales) === null || _a === void 0 ? void 0 : _a[routeLocale.value],
+        });
+    });
+    const facetFilters = [];
+    const initialize = () => {
+        var _a, _b;
+        facetFilters.splice(0, facetFilters.length, `lang:${lang.value}`, ...((_b = (_a = propsLocale.value.searchParameters) === null || _a === void 0 ? void 0 : _a.facetFilters) !== null && _b !== void 0 ? _b : []));
+        docsearch({
+            //...docsearchShim,
+            ...propsLocale.value,
+            container: '#docsearch-container',
+            searchParameters: {
+                ...propsLocale.value.searchParameters,
+                facetFilters,
+            },
+        });
+    };
+    onMounted(() => {
+        initialize();
+        // re-initialize if the options is changed
+        watch([routeLocale, propsLocale], ([curRouteLocale, curPropsLocale], [prevRouteLocale, prevPropsLocale]) => {
+            if (curRouteLocale === prevRouteLocale)
+                return;
+            if (JSON.stringify(curPropsLocale) !== JSON.stringify(prevPropsLocale)) {
+                initialize();
+            }
+        });
+        // modify the facetFilters in place to avoid re-initializing docsearch
+        // when page lang is changed
+        watch(lang, (curLang, prevLang) => {
+            if (curLang !== prevLang) {
+                const prevIndex = facetFilters.findIndex((item) => item === `lang:${prevLang}`);
+                if (prevIndex > -1) {
+                    facetFilters.splice(prevIndex, 1, `lang:${curLang}`);
+                }
+            }
+        });
+    });
+  },
+  render() {
+    return h('div', {id: 'docsearch-container'});
+  },
+};
+</script>
--- index.js
+++ index.js
@@ -88,6 +88,7 @@ module.exports = (options, app) => {
     name: '@lando/vuepress-theme-default-plus',
     extends: '@vuepress/theme-default',
     layouts: path.resolve(__dirname, 'layouts'),
+    clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.js'),
     plugins,
 
     // Add some page data
